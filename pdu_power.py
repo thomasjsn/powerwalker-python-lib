9988c6008da3075f73d5b8301641a9a4893460ee
Thomas Jensen <thomas@uctrl.net>
2018-03-20 08:50:14 +0100

    add pdu_power script

---
+++ pdu_power.py
@@ -0,0 +1,9 @@
+import powerwalker
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+# Define PDU and serial port, print status
+pdu = powerwalker.PDU("/dev/ttyUSB0")
+pp.pprint(pdu.power_watt())
+pp.pprint(pdu.power_va())
